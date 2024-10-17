bd8f79161adb7f2bb9c46909cb621765b794d2be
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2024-10-17 19:45:50 -0300

    Criacao do Projeto Spring Boot

---
+++ todo-simple-API/src/main/java/com/matheusmaia/todo_simple_API/TodoSimpleApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheusmaia.todo_simple_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSimpleApiApplication.class, args);
+	}
+
+}
---
+++ todo-simple-API/src/test/java/com/matheusmaia/todo_simple_API/TodoSimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmaia.todo_simple_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
